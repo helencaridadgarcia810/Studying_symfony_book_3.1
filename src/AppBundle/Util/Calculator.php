ad2d2752fcd3bca5ef72e0b658ac12bbe562ca05
Helen Caridad Garcia Gonzalez <helen.garcia@aleph.engineering>
2016-08-01 14:26:14 -0400

    Starting to do unit_testing with phpunit

---
+++ src/AppBundle/Util/Calculator.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: montells
+ * Date: 1/08/16
+ * Time: 13:53
+ */
+
+namespace AppBundle\Util;
+
+
+class Calculator
+{
+    public function add($a, $b)
+    {
+        return $a + $b;
+    }
+}
