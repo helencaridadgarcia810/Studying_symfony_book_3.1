bc0bc007c09155f4272c11c0e62e2d72dd58681e
Helen Caridad Garcia Gonzalez <helen.garcia@aleph.engineering>
2016-08-09 15:04:57 -0400

    The UserInterface Methods (Keep some Blank!) and Rendering of Login Form

--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -3,6 +3,7 @@
 namespace AppBundle\Entity;
 use Symfony\Component\Security\Core\Role\Role;
 use Symfony\Component\Security\Core\User\UserInterface;
+use Doctrine\ORM\Mapping as ORM;
 
 /**
  * @ORM\Entity
